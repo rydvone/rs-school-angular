49425377a7cfe1f9380942c043f3b8ef2e0d07db
rydvone <rydvone@gmail.com>
2022-10-01 00:24:23 +0300

    feat: add components and models

--- youtube-client/src/app/app.module.ts
+++ youtube-client/src/app/app.module.ts
@@ -3,9 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AuthComponent } from './auth/auth.component';
+import { SearchComponent } from './search/search.component';
+import { SettingsComponent } from './settings/settings.component';
+import { SortComponent } from './settings/sort/sort.component';
+import { FilterComponent } from './settings/filter/filter.component';
+import { CardsComponent } from './cards/cards.component';
+import { CardComponent } from './cards/card/card.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    AuthComponent,
+    SearchComponent,
+    SettingsComponent,
+    SortComponent,
+    FilterComponent,
+    CardsComponent,
+    CardComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
